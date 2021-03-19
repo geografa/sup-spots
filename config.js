2b8f7ad7e0f73ced5eb9cea707ee1f39792d4b41
Rafa Gutierrez <rafa@mapbox.com>
2021-03-18 22:48:50 -0700

    new icons

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var config = {
             title: 'Ross Island',
             image: './images/spots/ross-island.jpg',
             staticMap: true,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nunc at neque lacinia pharetra.',
+            description: 'If you\'re in Portland proper, then Ross Island is a must-visit. Getting to Ross Island is pretty straightforward and very accessible from either Willamette Park or Sellwood riverfront Park. There\'s lots of beach along Sellwood that you can hug for a while and if you\'re adventurous you can make it to the southern tip of Ross Island. Experienced paddlers will loop around the island for the 6 to 7 km trip or just hang out in the lagoon. This area gets really busy in the summer and lots of boats and jet skis will pass by. Be sure to stay out of the middle of the river and closer to shore (but watch out for remnant pilings as they will catch your fin and you\'ll be taking a dip).',
             location: {
                 center: [ -122.66577,45.47073],
                 zoom: 13.18,
@@ -39,10 +39,10 @@ var config = {
             title: 'Merwin Lake (Yale Bridge)',
             image: './images/spots/merwin.jpg',
             staticMap: true,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec nunc at neque lacinia pharetra. Integer tempus sagittis metus.',
+            description: 'Merwin is fave spot of mine. Just one hour away from Portland, you\'ll find yourself in a lush PNW forest, replete with fuzzy ferns and forest fairies. The parking is sparse at Yale Bridge so get there early. The staircase down to the launch is long and steep, guaranteed to scuff your board. But once you\'re in, it\'s a smooth ride to the pictureesque cascades awaiting your IG stories. And there\'s a rope swing under the bridge for the swinger in you.',
             location: {
-                center: [ -122.36715, 45.95728 ],
-                zoom: 13.57,
+                center: [-122.36976,45.95861],
+                zoom: 13,
                 pitch: 0.00,
                 bearing: 0.00
         },
@@ -64,10 +64,24 @@ var config = {
             title: 'Estacada Lake',
             image: './images/spots/estacada-lake.jpg',
             staticMap: true,
-            description: 'Estacada Lake (aka Timber Park, Milo McIver)',
+            description: 'Also within the 1 hour radius of Portland is Estacada Lake (aka Timber Park, Milo McIver SP). Estacada is approachable from either Milo or Timber Park. Just depends on if you want to pay the park fee. Timber Park has rentals from <a href="https://clackamasriveroutfitters.com/paddle-board-kayak-rentals/">Clackamas River Outfitters</a>. Estacada is also quite beautiful like Merwin with steep cliffs and a few cascades along the way. ',
             location: {
                 center: [-122.34706,45.29564],
                 zoom: 12.92,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'id-narrows',
+            title: 'Willamette Narrows',
+            image: './images/spots/narrows.jpg',
+            staticMap: true,
+            description: 'Ready to step it up?',
+            location: {
+                center: [-122.64920, 45.33594],
+                zoom: 12.94,
                 pitch: 0.00,
                 bearing: 0.00
             },
